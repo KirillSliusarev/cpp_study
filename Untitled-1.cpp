a30bd270b613d2abea1ce947105e23d056e5b5e7
KirillSliusarev <slsrv.kirill@gmail.com>
2022-09-09 12:36:47 +0300

    first program

---
+++ Untitled-1.cpp
@@ -0,0 +1,8 @@
+#include <iostream> //input output stream
+using namespace std; // namespace standart
+int main() // main func
+{
+    setlocale(LC_ALL, ""); // setting up lacale
+    cout << "первая \\строка\n"; // first output
+    cout << "\"вторая строка\""; // second output
+}
